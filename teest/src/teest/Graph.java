f2291b8ff7c40148140aa895f1d09633a1a53487
Rana Osama <66523119+RanaUsama20@users.noreply.github.com>
2022-05-24 01:04:06 +0200

    Update Graph.java

--- teest/src/teest/Graph.java
+++ teest/src/teest/Graph.java
@@ -12,7 +12,7 @@ public class Graph {
   int key;
   Map<Integer, ArrayList<Integer>> graph = new HashMap<Integer, ArrayList<Integer>>();
   ArrayList<Integer> list = new ArrayList<Integer>();
-  ArrayList<obj> Key_followers = new ArrayList<obj>();
+  ArrayList<obj> Key_followers = new ArrayList<obj>(); 
 
   // Build HashMap
 
@@ -21,17 +21,17 @@ public class Graph {
     if (graph.containsKey(k)) { // check if account id is already exist (added before)
 
       ArrayList<Integer> followers;
-      followers = graph.get(k); // get arraylist of his followers
+      followers = graph.get(k); // get arraylist of its followers
 
       if (!followers.contains(v)) { // check if this follower isn't already added before in arraylist
 
         counter = followers.remove(followers.size() - 1); // get number of followers for this account (we select last
                                                           // index of each arraylist for counter of followers in this
                                                           // arraylist)
-        followers.add(v); // add follower id
+        followers.add(v); // add follower value
 
         counter++; // number of followers++
-        followers.add(counter); // add counter at last index of arraylist after updating number of followers
+        followers.add(counter); // add counter at last index of arraylist after updating number of followers (values)
         graph.put(k, followers); // add account id and arraylist of his followers in " key / value " pairs
 
       }
@@ -67,19 +67,17 @@ public class Graph {
   public void TopInfluencer(int i) {
 
     Set<Integer> Kset = graph.keySet();
-    ArrayList<Integer> sortFollowers = new ArrayList<Integer>(); // Create ArrayList that carry number of followers for
-                                                                 // each account(key)
+    ArrayList<Integer> sortFollowers = new ArrayList<Integer>(); // Create ArrayList that carry number of followers for each account(key)
 
     for (Object j : Kset) {
 
       ArrayList<Integer> value;
       value = graph.get(j); // Store list of followers for each key per itretion
-      int c = value.remove(value.size() - 1); // Get the Number of follwers for each key that always in the last of the
-                                              // list
-      sortFollowers.add(c);
+      int c = value.remove(value.size() - 1); // Get the Number of followers for each key that always in the last of the list
+      sortFollowers.add(c); // adding c to the arraylist
 
       obj e = new obj((Integer) j, c);
-      Key_followers.add(e);
+      Key_followers.add(e);   //key_followers is an array of objects that carry the key and the number of its followers(counter)
       value.add(c);
       graph.put((Integer) j, value);
 
@@ -88,13 +86,14 @@ public class Graph {
     Collections.sort(sortFollowers); // Built in sort time complexity is O(n*log(n)) - Sort Element in ascending
                                      // order
 
-    int f = sortFollowers.get(sortFollowers.size() - i); // Store the number of followers for the key i passed to the
-                                                         // function in f
-
-    for (obj element : Key_followers) { // As sorting number of followers calculated seperately
-                                        // we need to get the key related to that number of followers that stored in f.
-      if (element.counter == f) { // by looping on each key stored in key_followers(array of objects that carry
-                                  // the key and the number of its followers(counter)).
+    int f = sortFollowers.get(sortFollowers.size() - i); // Store the number of followers for the key i passed to the function in f.
+    
+    
+// As sorting number of followers calculated seperately, we need to get the key related to that number of followers that stored in f.
+// by looping on each key stored in key_followers(array of objects that carry the key and the number of its followers(counter).
+   
+    for (obj element : Key_followers) { 
+      if (element.counter == f) { 
         int k = element.key;
 
         key = k;
