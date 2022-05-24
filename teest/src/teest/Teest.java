93eb8ec794a26bf6f6e3354971fedf202fc87395
Mariam-Fathi <66276072+Mariam-Fathi@users.noreply.github.com>
2022-05-24 06:02:13 +0200

    Update Teest.java

--- teest/src/teest/Teest.java
+++ teest/src/teest/Teest.java
@@ -38,49 +38,71 @@ public class Teest {
             }
         }
 
-        System.out.println("Number of accounts = " + g.sizee());
-        System.out.println();
-        System.out.print("Top Influencer ");
-        g.TopInfluencer(1);
+        //       System.out.println("Number of accounts = "+g.sizee());
+//       System.out.println();  
+//       System.out.println();
+//       System.out.print("Top Influencer ");
+//       g.TopInfluencer(1);
+//       System.out.println();  
+//       System.out.println();
+//       System.out.print("Second ");
+//       g.TopInfluencer(2);
+//       System.out.println();  
+//       System.out.println();
+//       System.out.print("Third ");
+//       g.TopInfluencer(3);
+//       System.out.println();  
+//       System.out.println();
 
-        recommendation2 r = new recommendation2(12, g);
-        r.MaxLength();
-        // System.out.println();
-        // System.out.print("Second ");
-        // g.TopInfluencer(2);
-        // System.out.println();
-        // System.out.print("Third ");
-        // g.TopInfluencer(3);
-        // System.out.println();
 
-        // Bonus..
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        // int id1=115485051; //aleady follow
-        // int id2=40981798;
+       
+       //Bonus..
+      
+//            int id1=115485051;        // suggest    
+//            int id2=40981798;
 
-        // int id1=358083488; //no action
-        // int id2=400689940;
 
-        // int id1=303060652; //aleady follow
-        // int id2=394407073;
 
-        // int id1=294198566;
-        // int id2=294198566;
+//           int id1=303060652;         //aleady follow
+//           int id2=394407073;
+ 
+       
+//             int id1=294198566;         //No Action  
+//             int id2=40981798;
+             
+//           int id1=294198566;           
+//           int id2=294198566;
 
-        // int id1=43003845; //new account suggestion to follow
-        // int id2=43003845;
 
-        // ArrayList<Integer> account1 = g.graph.get(id1);
-        // ArrayList<Integer> account2 = g.graph.get(id2);
 
-        // int m = account1.size();
-        // int n = account2.size();
 
-        // recommendation o = new recommendation();
 
-        // o.Print(id1,id2,account1,account2,m,n);
+//            ArrayList<Integer> account1 = g.graph.get(id1);
+//            ArrayList<Integer> account2 = g.graph.get(id2);
+//            
+//            int m = account1.size();
+//            int n = account2.size();
+//        
+//            recommendation o = new recommendation();
+//            o.Print(id1,id2,account1,account2,m,n);
+//           
 
-        // System.out.println(g.num_followers(294198566));
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//            int id=532655718;
+//            recommendation2 o = new recommendation2(id,g);
+//            o.MaxLength();
+
+//            System.out.println(g.graph.get(61989945));
+//            System.out.println(g.graph.get(532655718));
+//        
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     }
 
