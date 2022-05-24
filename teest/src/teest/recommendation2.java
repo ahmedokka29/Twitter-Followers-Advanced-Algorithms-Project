1dcc818afbd061841cc3d38d5a30a9594d82b643
Mariam-Fathi <66276072+Mariam-Fathi@users.noreply.github.com>
2022-05-24 05:26:28 +0200

    Create recommendation2.java

---
+++ teest/src/teest/recommendation2.java
@@ -0,0 +1,44 @@
+
+package teest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class recommendation2 {
+    
+        int id;
+        recommendation o = new recommendation();
+        Graph g = new Graph();   
+        Map<Integer,ArrayList<Integer>> r  = new HashMap<Integer,ArrayList<Integer>>();
+        Set <Integer> kset;
+
+    public recommendation2(int id,Graph g) {
+        this.id = id;
+        this.g=g;
+        this.r = g.graph;
+        this.kset = r.keySet();
+    }
+        
+        
+        
+       public void MaxLength()
+            {
+               for (Object i : kset) {
+                   
+                   if (o.MaxLength(id, (Integer) i, r.get(id), r.get(i),r.get(id).size(),r.get(i).size()) >10){
+                        System.out.println(id+", You have a new account suggestion to follow -->  "+(Integer) i);
+                        System.out.println(o.MaxLength(id,(Integer) i,r.get(id), r.get(i), r.get(id).size() ,r.get(i).size())+ "  mutual followers");
+                         System.out.println(); 
+               
+                   }
+                   
+                  
+            }
+               
+            }     
+
+    
+
+}
