41142814d035e1931508abfee0b30d43f6c4891f
Ahmed Gamal Okka <56696104+ahmedokka29@users.noreply.github.com>
2022-05-24 06:02:53 +0200

    Update Teest.java

--- teest/src/teest/Teest.java
+++ teest/src/teest/Teest.java
@@ -38,71 +38,59 @@ public class Teest {
             }
         }
 
-        //       System.out.println("Number of accounts = "+g.sizee());
-//       System.out.println();  
-//       System.out.println();
-//       System.out.print("Top Influencer ");
-//       g.TopInfluencer(1);
-//       System.out.println();  
-//       System.out.println();
-//       System.out.print("Second ");
-//       g.TopInfluencer(2);
-//       System.out.println();  
-//       System.out.println();
-//       System.out.print("Third ");
-//       g.TopInfluencer(3);
-//       System.out.println();  
-//       System.out.println();
+        // System.out.println("Number of accounts = "+g.sizee());
+        // System.out.println();
+        // System.out.println();
+        // System.out.print("Top Influencer ");
+        // g.TopInfluencer(1);
+        // System.out.println();
+        // System.out.println();
+        // System.out.print("Second ");
+        // g.TopInfluencer(2);
+        // System.out.println();
+        // System.out.println();
+        // System.out.print("Third ");
+        // g.TopInfluencer(3);
+        // System.out.println();
+        // System.out.println();
 
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Bonus..
 
-       
-       //Bonus..
-      
-//            int id1=115485051;        // suggest    
-//            int id2=40981798;
+        // int id1=115485051; // suggest
+        // int id2=40981798;
 
+        // int id1=303060652; //aleady follow
+        // int id2=394407073;
 
+        // int id1=294198566; //No Action
+        // int id2=40981798;
 
-//           int id1=303060652;         //aleady follow
-//           int id2=394407073;
- 
-       
-//             int id1=294198566;         //No Action  
-//             int id2=40981798;
-             
-//           int id1=294198566;           
-//           int id2=294198566;
+        // int id1=294198566;
+        // int id2=294198566;
 
+        // ArrayList<Integer> account1 = g.graph.get(id1);
+        // ArrayList<Integer> account2 = g.graph.get(id2);
+        //
+        // int m = account1.size();
+        // int n = account2.size();
+        //
+        // recommendation o = new recommendation();
+        // o.Print(id1,id2,account1,account2,m,n);
+        //
 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+        // int id=532655718;
+        // recommendation2 o = new recommendation2(id,g);
+        // o.MaxLength();
 
+        // System.out.println(g.graph.get(61989945));
+        // System.out.println(g.graph.get(532655718));
+        //
 
-//            ArrayList<Integer> account1 = g.graph.get(id1);
-//            ArrayList<Integer> account2 = g.graph.get(id2);
-//            
-//            int m = account1.size();
-//            int n = account2.size();
-//        
-//            recommendation o = new recommendation();
-//            o.Print(id1,id2,account1,account2,m,n);
-//           
-
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-//            int id=532655718;
-//            recommendation2 o = new recommendation2(id,g);
-//            o.MaxLength();
-
-//            System.out.println(g.graph.get(61989945));
-//            System.out.println(g.graph.get(532655718));
-//        
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     }
 
